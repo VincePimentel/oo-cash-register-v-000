5057811ec0b76193bf56752645cb19b0276aaa32
Vince Andrew Pimentel <contact@vincep.me>
2018-10-23 04:03:17 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 class CashRegister
   attr_accessor :discount, :total#, :add_item, :apply_discount, :items, :void_last_transaction
 
-  def initialize(discount)
+  def initialize
     @discount = discount
     @total = 0
   end
