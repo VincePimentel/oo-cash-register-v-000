94fed99433a7884a027fe90674fac215db669dff
Vince Andrew Pimentel <contact@vincep.me>
2018-10-23 04:33:38 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,6 +11,6 @@ class CashRegister
   end
 
   def apply_discount
-    self.total = self.total.to_f - self.total.to_f * self.discount.to_f
+    self.total = self.total.to_i - self.total.to_i * self.discount.to_i
   end
 end
